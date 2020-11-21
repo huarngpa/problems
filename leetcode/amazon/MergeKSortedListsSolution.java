50a85db092f5f3c3bdf542e54b4f96b96a1a2ae3
Patrick Huarng <huarngpa@gmail.com>
2020-11-21 17:56:09 -0600

    chore(docs): add prompts

--- leetcode/amazon/MergeKSortedListsSolution.java
+++ leetcode/amazon/MergeKSortedListsSolution.java
@@ -1,5 +1,13 @@
 import java.util.*;
 
+/**
+ * Merge k Sorted Lists
+ * 
+ * You are given an array of k linked-lists lists, each linked-list is sorted in
+ * ascending order.
+ * 
+ * Merge all the linked-lists into one sorted linked-list and return it.
+ */
 public class MergeKSortedListsSolution {
 
   public static class ListNode {
