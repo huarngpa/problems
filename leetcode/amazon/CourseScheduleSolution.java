e5b1ca334b0b0d52738c730b4a24f2b2ef1e8bfa
Patrick Huarng <huarngpa@gmail.com>
2020-11-25 21:00:06 -0600

    chore(amzn): problem notes

--- leetcode/amazon/CourseScheduleSolution.java
+++ leetcode/amazon/CourseScheduleSolution.java
@@ -1,5 +1,19 @@
 import java.util.*;
 
+/**
+ * Course Schedule II
+ * 
+ * There are a total of n courses you have to take labelled from 0 to n - 1.
+ * 
+ * Some courses may have prerequisites, for example, if prerequisites[i] = [ai,
+ * bi] this means you must take the course bi before the course ai.
+ * 
+ * Given the total number of courses numCourses and a list of the prerequisite
+ * pairs, return the ordering of courses you should take to finish all courses.
+ * 
+ * If there are many valid answers, return any of them. If it is impossible to
+ * finish all courses, return an empty array.
+ */
 public class CourseScheduleSolution {
   public class Rank {
     int value = 0;
