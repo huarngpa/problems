7dd0b2550bd7e6a1132a6be8b0285a3e4cf2d815
Patrick Huarng <huarngpa@gmail.com>
2022-05-20 21:50:40 -0400

    Google subarray sums optimized solution

--- leetcode/google/SubarraySumsEqualsK.java
+++ leetcode/google/SubarraySumsEqualsK.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarraySumsEqualsK {
   // https://leetcode.com/problems/subarray-sum-equals-k/discuss/803317/Java-Solution-with-Detailed-Explanation
   public int subarraySum(int[] nums, int k) {
@@ -18,6 +21,28 @@ public class SubarraySumsEqualsK {
           count++;
         }
       }
+    }
+    return count;
+  }
+
+  /**
+   * An O(n) time solution, but you need to understand the previous solution
+   * and exploit the relationships.
+   */
+  public int subarraySumOptimized(int[] nums, int k) {
+    int count = 0;
+    int runningSum = 0;
+    // Stores the occurence of subarray sums we've seen thus far
+    Map<Integer, Integer> occurrences = new HashMap<>();
+    // Zero by default occurs once (use the current subarray sum)
+    occurrences.put(0, 1);
+    for (int i = 0; i < nums.length; i++) {
+      runningSum += nums[i];
+      // Use the relationship: sums[end] - sums[start] = k
+      // You use the occurences seen because N subarrays seen
+      // satisfy this condition.
+      count += occurrences.getOrDefault(runningSum - k, 0);
+      occurrences.put(runningSum, occurrences.getOrDefault(runningSum, 0) + 1);
     }
     return count;
   }
