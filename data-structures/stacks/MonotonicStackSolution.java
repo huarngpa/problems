28479d3e10e2788779224849c6ed048c4d47433f
Patrick Huarng <huarnp@amazon.com>
2022-07-17 11:53:10 -0400

    Monotonic stack algorithm

---
+++ data-structures/stacks/MonotonicStackSolution.java
@@ -0,0 +1,28 @@
+package stacks;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class MonotonicStackSolution {
+
+  public static void main(String[] args) {
+
+    List<Integer> list = Arrays.asList(3, 1, 6, 2, 5, 4);
+
+    // Create a monotonic stack
+    Stack<Integer> stack = new Stack<>();
+    for (Integer num : list) {
+      while (!stack.isEmpty() && num >= stack.peek()) {
+        stack.pop();
+      }
+      stack.add(num);
+      System.out.println(String.format("Num=%d Stack=%s", num, stack));
+    }
+
+    // A "monotonic" property is maintained and this has useful implications. For
+    // instance, in the area of a histogram problem we need to know where the
+    // previous smaller number/index exists. What other problems might a monotonic
+    // stack come in handy?
+  }
+}
